6d318578885ccced9ca6c6b949b3101434a16db0
iamstyx <vtechnical1804@gmail.com>
2024-02-16 23:27:41 +0530

    Update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt-nyoa.onrender.com", {
+			const socket = io("https://chat-app-yt-g1fy.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
